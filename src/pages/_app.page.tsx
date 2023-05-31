ae07cd3296cab8d420c28d5227ab9dca461a69a1
RodolfoBohn <ensino.rodolfofbohn@gmail.com>
2023-05-31 17:12:13 -0300

    Autenticacao oAuth com google e nextauth

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,14 +1,20 @@
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 import { Roboto } from 'next/font/google'
 import { globalStyles } from '@/styles/global'
 
 const roboto = Roboto({ weight: ['400', '500', '700'], subsets: ['latin'] })
 
 globalStyles()
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
     <div className={roboto.className}>
-      <Component {...pageProps} />
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </div>
   )
 }
